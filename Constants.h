565e4e690fd234dae054dc365798b8dd64ed399e
Adam Jensen <acj@linuxguy.org>
2014-03-16 18:10:28 -0400

    Add Constants class to define the preferred network interface

---
+++ Constants.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+FOUNDATION_EXPORT NSString *const UseNetworkInterface;
+
+@end
---
+++ Constants.m
@@ -0,0 +1,8 @@
+#import "Constants.h"
+
+@implementation Constants
+
+//NSString *const UseNetworkInterface = @"en0"; // Ethernet
+NSString *const UseNetworkInterface = @"en1"; // Wifi
+
+@end
